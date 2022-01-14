a3fe8c677315104246bb7410048406badaf85218
Rudragithub2 <92922468+Rudragithub2@users.noreply.github.com>
2022-01-14 11:54:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function dogcat (){
+  window.location = "dog_cat.html";
+}
+function bedroom (){
+    window.location = "bedroom.html";
+  }
+  function drawingroom (){
+    window.location = "drawingroom.html";
+  }
+  function studytable (){
+    window.location = "studytable.html";
+  }
